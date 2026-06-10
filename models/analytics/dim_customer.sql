a4edf6a00bff5cc885f3fe009ae60d7e85db6fc8
bao <felixbaolab@gmail.com>
2026-06-10 16:33:20 +0700

    add columns

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,7 +7,8 @@ with source_data as (
   customer_id AS customer_key,
   customer_name,
   customer_category_id as customer_category_key,
-  buying_group_id as buying_group_key
+  buying_group_id as buying_group_key,
+  is_on_credit_hold
   FROM source_data
 )
 , cast_type as (
@@ -15,8 +16,20 @@ with source_data as (
   CAST(customer_key AS INTEGER) AS customer_key,
   CAST(customer_name AS STRING) AS customer_name,
   CAST(customer_category_key AS INTEGER) AS customer_category_key,
-  CAST(buying_group_key AS INTEGER) AS buying_group_key
+  CAST(buying_group_key AS INTEGER) AS buying_group_key,
+  CAST(is_on_credit_hold AS BOOLEAN) AS is_on_credit_hold_boolean
   FROM rename_columns
+)
+, convert_boolean AS (
+  SELECT
+  *,
+  CASE 
+      WHEN is_on_credit_hold_boolean is TRUE THEN 'On Credit Hold' 
+      WHEN is_on_credit_hold_boolean is FALSE THEN 'Not On Credit Hold' 
+      WHEN is_on_credit_hold_boolean IS NULL THEN 'Undefined'
+      ELSE 'Invalid'
+      END AS is_on_credit_hold
+  FROM cast_type
 )
 select 
 customer.customer_key,
@@ -24,8 +37,9 @@ customer.customer_name,
 customer.customer_category_key,
 customer_category.customer_category_name,
 customer.buying_group_key,
-buying_group.buying_group_name
-from cast_type as customer
+buying_group.buying_group_name,
+customer.is_on_credit_hold
+from convert_boolean as customer
 left join {{ref('stg_dim_customer_categories')}} customer_category
 on customer.customer_category_key = customer_category.customer_category_key
 left join {{ref('stg_dim_buying_group')}} buying_group
--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -8,7 +8,8 @@ with source_data AS (
   stock_item_id AS product_key,
   stock_item_name AS product_name,
   brand AS brand_name,
-  supplier_id AS supplier_key
+  supplier_id AS supplier_key,
+  is_chiller_stock
   FROM source_data
 )
 , cast_type AS (
@@ -16,16 +17,29 @@ with source_data AS (
   CAST(product_key AS INTEGER) AS product_key,
   CAST(product_name AS STRING) AS product_name,
   CAST(brand_name AS STRING) AS brand_name,
-  CAST(supplier_key AS INTEGER) AS supplier_key
+  CAST(supplier_key AS INTEGER) AS supplier_key,
+  cast(is_chiller_stock AS BOOLEAN) AS is_chiller_stock_boolean
   FROM rename_columns
+)
+, convert_boolean AS (
+  SELECT
+  *,
+  CASE 
+      WHEN is_chiller_stock_boolean is TRUE THEN 'Chiller Stock' 
+      WHEN is_chiller_stock_boolean is FALSE THEN 'Non-Chiller Stock' 
+      WHEN is_chiller_stock_boolean IS NULL THEN 'Undefined'
+      ELSE 'Invalid'
+      END AS is_chiller_stock
+  FROM cast_type
 )
 SELECT  
 product.product_key,
 product.product_name,
 product.brand_name,
 product.supplier_key,
-supplier.supplier_name
+supplier.supplier_name,
+product.is_chiller_stock
 FROM 
-cast_type product
+convert_boolean product
 left join {{ref('dim_supplier')}} supplier
 on product.supplier_key = supplier.supplier_key
